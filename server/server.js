56ee4d204e08808257e8ae1adfa7b7147e1d0338
jotavalla <jimmy.valladares@epn.edu.ec>
2025-05-23 13:31:44 -0500

    add structure of the project

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const corsOptions = {
+    origin: ["http://localhost:5173"],
+};
+
+app.use(cors(corsOptions));
+
+
+app.get("/api", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(8080, () => {
+  console.log("Server is running on port 8080");
+});
+
